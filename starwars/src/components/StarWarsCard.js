d8a3c141c2980fa7c102a839da372b4b3c5f4b17
korynewton <korynewtondev@gmail.com>
2019-02-15 09:19:47 -0800

    more styling added

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -4,13 +4,13 @@ const StarWarsCard = (props) => {
     console.log(props.info)
     return (
         <div className={'card'}>
-            <h2><strong>Name:</strong> {props.info.name}</h2>
-            <h4><strong>Gender:</strong> {props.info.gender}</h4>
-            <h4><strong>Birth Year:</strong> {props.info.birth_year}</h4>
-            <h4><strong>Hair Color:</strong> {props.info.hair_color}</h4>
-            <h4><strong>Mass:</strong> {props.info.mass}kg</h4>
-            <h4><strong>Eye Color:</strong> {props.info.eye_color}</h4>
-            <h4><strong>Skin Color:</strong> {props.info.skin_color}</h4>
+            <h2>{props.info.name}</h2>
+            <p><strong>Gender:</strong> {props.info.gender}</p>
+            <p><strong>Birth Year:</strong> {props.info.birth_year}</p>
+            <p><strong>Hair Color:</strong> {props.info.hair_color}</p>
+            <p><strong>Mass:</strong> {props.info.mass}kg</p>
+            <p><strong>Eye Color:</strong> {props.info.eye_color}</p>
+            <p><strong>Skin Color:</strong> {props.info.skin_color}</p>
         </div>
     )
 }
